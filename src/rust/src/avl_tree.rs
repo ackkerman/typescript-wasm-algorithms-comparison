7a06004a03182b64412fbd500648eb6ea7de4b3d
moxak <moxak.b@gmail.com>
2025-05-16 02:20:23 +0900

    feat/avl_tree insert_many 内でソート＆重複排除（sort_unstable + dedup）

--- src/rust/src/avl_tree.rs
+++ src/rust/src/avl_tree.rs
@@ -3,122 +3,156 @@ use std::cmp::max;
 
 #[wasm_bindgen]
 pub struct AVLTree {
-    root: Option<Box<Node>>,
+    nodes: Vec<Node>,
+    root: Option<usize>,
 }
 
 #[derive(Clone)]
 struct Node {
     value: i32,
     height: i32,
-    left: Option<Box<Node>>,
-    right: Option<Box<Node>>,
+    left: Option<usize>,
+    right: Option<usize>,
 }
 
 #[wasm_bindgen]
 impl AVLTree {
     #[wasm_bindgen(constructor)]
     pub fn new() -> Self {
-        AVLTree { root: None }
+        AVLTree { nodes: Vec::new(), root: None }
     }
 
     #[wasm_bindgen]
     pub fn insert_many(&mut self, values: &[i32]) {
-        for &v in values {
-            let root = self.root.take();
-            self.root = self.insert_node(root, v);
+        let mut values = values.to_vec();
+        values.sort_unstable();
+        values.dedup();
+        for &v in &values {
+            self.root = Some(self.insert_node(self.root, v));
         }
     }
 
     #[wasm_bindgen]
     pub fn search_many(&self, values: &[i32]) -> Vec<u8> {
-        values.iter().map(|&v| self.search_node(&self.root, v) as u8).collect()
+        values.iter().map(|&v| self.search_node(self.root, v) as u8).collect()
     }
 
     #[wasm_bindgen]
     pub fn free(&mut self) {
+        self.nodes.clear();
         self.root = None;
     }
 
-    fn insert_node(&self, node: Option<Box<Node>>, value: i32) -> Option<Box<Node>> {
-        match node {
-            None => Some(Box::new(Node {
-                value,
-                height: 1,
-                left: None,
-                right: None,
-            })),
-            Some(mut n) => {
-                if value < n.value {
-                    n.left = self.insert_node(n.left.take(), value);
-                } else if value > n.value {
-                    n.right = self.insert_node(n.right.take(), value);
+    fn insert_node(&mut self, node_idx: Option<usize>, value: i32) -> usize {
+        match node_idx {
+            None => {
+                let idx = self.nodes.len();
+                self.nodes.push(Node { value, height: 1, left: None, right: None });
+                idx
+            },
+            Some(idx) => {
+                let mut node = self.nodes[idx].clone();
+                if value < node.value {
+                    node.left = Some(self.insert_node(node.left, value));
+                } else if value > node.value {
+                    node.right = Some(self.insert_node(node.right, value));
                 } else {
-                    return Some(n);
+                    return idx;
                 }
-                Self::update_height(&mut n);
-                Some(Self::rebalance(n))
+
+                Self::update_height(&mut node, &self.nodes);
+                let rebalanced_idx = self.rebalance(node, idx);
+                rebalanced_idx
             }
         }
     }
 
-    fn search_node(&self, node: &Option<Box<Node>>, value: i32) -> bool {
-        match node {
+    fn search_node(&self, node_idx: Option<usize>, value: i32) -> bool {
+        match node_idx {
             None => false,
-            Some(n) => match value.cmp(&n.value) {
-                std::cmp::Ordering::Equal => true,
-                std::cmp::Ordering::Less => self.search_node(&n.left, value),
-                std::cmp::Ordering::Greater => self.search_node(&n.right, value),
-            },
+            Some(idx) => {
+                let node = &self.nodes[idx];
+                match value.cmp(&node.value) {
+                    std::cmp::Ordering::Equal => true,
+                    std::cmp::Ordering::Less => self.search_node(node.left, value),
+                    std::cmp::Ordering::Greater => self.search_node(node.right, value),
+                }
+            }
         }
     }
 
-    fn height(node: &Option<Box<Node>>) -> i32 {
-        node.as_ref().map_or(0, |n| n.height)
+    fn height(nodes: &Vec<Node>, idx: Option<usize>) -> i32 {
+        idx.map_or(0, |i| nodes[i].height)
     }
 
-    fn update_height(node: &mut Box<Node>) {
-        node.height = 1 + max(Self::height(&node.left), Self::height(&node.right));
+    fn update_height(node: &mut Node, nodes: &Vec<Node>) {
+        node.height = 1 + max(Self::height(nodes, node.left), Self::height(nodes, node.right));
     }
 
-    fn balance_factor(node: &Box<Node>) -> i32 {
-        Self::height(&node.left) - Self::height(&node.right)
+    fn balance_factor(node: &Node, nodes: &Vec<Node>) -> i32 {
+        Self::height(nodes, node.left) - Self::height(nodes, node.right)
     }
 
-    fn rebalance(mut node: Box<Node>) -> Box<Node> {
-        let balance = Self::balance_factor(&node);
+    fn rebalance(&mut self, mut node: Node, original_idx: usize) -> usize {
+        let balance = Self::balance_factor(&node, &self.nodes);
 
         if balance > 1 {
-            if Self::balance_factor(node.left.as_ref().unwrap()) < 0 {
-                node.left = Some(Self::rotate_left(node.left.take().unwrap()));
+            if Self::balance_factor(&self.nodes[node.left.unwrap()], &self.nodes) < 0 {
+                node.left = Some(self.rotate_left(node.left.unwrap()));
             }
-            return Self::rotate_right(node);
+            return self.rotate_right_node(node);
         }
 
         if balance < -1 {
-            if Self::balance_factor(node.right.as_ref().unwrap()) > 0 {
-                node.right = Some(Self::rotate_right(node.right.take().unwrap()));
+            if Self::balance_factor(&self.nodes[node.right.unwrap()], &self.nodes) > 0 {
+                node.right = Some(self.rotate_right(node.right.unwrap()));
             }
-            return Self::rotate_left(node);
+            return self.rotate_left_node(node);
         }
 
-        node
+        self.nodes[original_idx] = node;
+        original_idx
     }
 
-    fn rotate_left(mut x: Box<Node>) -> Box<Node> {
-        let mut y = x.right.take().unwrap();
-        x.right = y.left.take();
-        Self::update_height(&mut x);
-        y.left = Some(x);
-        Self::update_height(&mut y);
-        y
+    fn rotate_left(&mut self, x_idx: usize) -> usize {
+        let mut x = self.nodes[x_idx].clone();
+        let y_idx = x.right.unwrap();
+        let mut y = self.nodes[y_idx].clone();
+
+        x.right = y.left;
+        Self::update_height(&mut x, &self.nodes);
+        y.left = Some(x_idx);
+        Self::update_height(&mut y, &self.nodes);
+
+        self.nodes[x_idx] = x;
+        self.nodes[y_idx] = y;
+        y_idx
     }
 
-    fn rotate_right(mut y: Box<Node>) -> Box<Node> {
-        let mut x = y.left.take().unwrap();
-        y.left = x.right.take();
-        Self::update_height(&mut y);
-        x.right = Some(y);
-        Self::update_height(&mut x);
-        x
+    fn rotate_right(&mut self, y_idx: usize) -> usize {
+        let mut y = self.nodes[y_idx].clone();
+        let x_idx = y.left.unwrap();
+        let mut x = self.nodes[x_idx].clone();
+
+        y.left = x.right;
+        Self::update_height(&mut y, &self.nodes);
+        x.right = Some(y_idx);
+        Self::update_height(&mut x, &self.nodes);
+
+        self.nodes[y_idx] = y;
+        self.nodes[x_idx] = x;
+        x_idx
+    }
+
+    fn rotate_left_node(&mut self, node: Node) -> usize {
+        let idx = self.nodes.len();
+        self.nodes.push(node);
+        self.rotate_left(idx)
+    }
+
+    fn rotate_right_node(&mut self, node: Node) -> usize {
+        let idx = self.nodes.len();
+        self.nodes.push(node);
+        self.rotate_right(idx)
     }
 }
